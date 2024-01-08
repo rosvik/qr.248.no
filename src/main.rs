d4eb61ec9bfa376ab3a413703c34b22f9834bd54
Johannes Røsvik <j.rosvik@gmail.com>
2024-01-09 00:52:41 +0100

    refactor to use qrcode crate

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use axum::{
     Router,
 };
 use http::HeaderValue;
-use qrcode_generator::QrCodeEcc;
+use image::{codecs::png::PngEncoder, ColorType, ImageEncoder, Luma};
+use qrcode::QrCode;
 use serde::{de, Deserialize, Deserializer};
 use std::{fmt, net::SocketAddr, str::FromStr};
 
@@ -37,20 +38,35 @@ struct GetQrParameters {
 async fn get_qr(Path(_): Path<String>, Query(query): Query<GetQrParameters>) -> impl IntoResponse {
     let size = query.size.unwrap_or(1024).min(4096);
     let data = query.data;
-    println!("Generated QR code for '{}' at {}px", data, size);
+    println!("Generating QR code for '{}' at {}px", data, size);
 
-    let result: Vec<u8> = qrcode_generator::to_png_to_vec(data, QrCodeEcc::Low, size as usize)
-        .unwrap_or_else(|e| {
-            eprintln!("Error generating QR code: {}", e);
-            vec![]
-        });
+    let qrcode = QrCode::new(data).unwrap();
+
+    // TODO: Add more options here (colors and quiet zone)
+    let image: image::ImageBuffer<Luma<u8>, Vec<u8>> = qrcode
+        .render::<Luma<u8>>()
+        .min_dimensions(size, size)
+        .build();
+    let width = image.width();
+    let height = image.height();
+
+    // TODO: Support other image formats and SVG
+    let mut result_bytes: Vec<u8> = Vec::new();
+    let encoder: PngEncoder<&mut Vec<u8>> = PngEncoder::new(&mut result_bytes);
+    match encoder.write_image(&image.into_raw(), width, height, ColorType::L8) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Failed to encode image: {}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, HeaderMap::new(), vec![]);
+        }
+    }
 
     let mut image_headers = HeaderMap::new();
     image_headers.insert(
         http::header::CONTENT_TYPE,
         HeaderValue::from_static("image/png"),
     );
-    (StatusCode::OK, image_headers, result)
+    (StatusCode::OK, image_headers, result_bytes)
 }
 
 fn empty_string_as_none<'de, D, T>(de: D) -> Result<Option<T>, D::Error>
